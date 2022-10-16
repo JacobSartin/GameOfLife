2cc0d30779826591af2f22c98aef67bf589182f6
JacobSartin <59787306+JacobSartin@users.noreply.github.com>
2022-10-16 15:26:00 -0500

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+import java.awt.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Main");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLayout(new BorderLayout());
+        frame.add(new GamePanel(), BorderLayout.CENTER);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+    }
+}
